1757283e94008e6583656aa7d5bd2f75f4db2595
Sahas Timilsina <tsahas2059@gmail.com>
2025-07-01 17:44:26 +0545

    vector for coordinates in 3d plane

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,43 @@ const Color = packed struct {
     rgb: RGB,
 };
 
+const Vec3 = struct {
+    data: @Vector(3, f64),
+
+    pub fn init(a: f64, b: f64, c: f64) Vec3 {
+        return Vec3{ .data = @Vector(3, f64){ a, b, c } };
+    }
+
+    pub fn x(self: Vec3) f64 {
+        return self.data[0];
+    }
+    pub fn y(self: Vec3) f64 {
+        return self.data[1];
+    }
+    pub fn z(self: Vec3) f64 {
+        return self.data[2];
+    }
+
+    pub fn setX(self: *Vec3, ordinate: f64) void {
+        self.data[0] = ordinate;
+    }
+    pub fn setY(self: *Vec3, ordinate: f64) void {
+        self.data[1] = ordinate;
+    }
+    pub fn setZ(self: *Vec3, ordinate: f64) void {
+        self.data[2] = ordinate;
+    }
+    pub fn add(self: Vec3, other: Vec3) Vec3 {
+        return Vec3{
+            .data = .{
+                self.data[0] + other.data[0],
+                self.data[1] + other.data[1],
+                self.data[2] + other.data[2],
+            },
+        };
+    }
+};
+
 const PPM = struct {
     image_width: usize,
     image_height: usize,
@@ -25,17 +62,22 @@ const PPM = struct {
         self.allocator.free(self.data);
     }
     pub fn saveInFile(self: *PPM, filename: []const u8) !void {
-        var file = try std.fs.cwd().openFile(filename, .{ .mode = .write_only });
+        var file = try std.fs.cwd().createFile(filename, .{});
         defer file.close();
         errdefer file.close();
 
-        var fwriter = file.writer();
-        try fwriter.print("P3\n{} {}\n255\n", .{ self.image_width, self.image_height });
+        const fwriter = file.writer();
+
+        var bufferedWriter = std.io.bufferedWriter(fwriter);
+        var bwriter = bufferedWriter.writer();
+
+        try bwriter.print("P3\n{} {}\n255\n", .{ self.image_width, self.image_height });
 
         for (self.data) |pixel| {
             // Output color
-            try fwriter.print("{} {} {}\n", .{ pixel.rgb.r, pixel.rgb.g, pixel.rgb.b });
+            try bwriter.print("{} {} {}\n", .{ pixel.rgb.r, pixel.rgb.g, pixel.rgb.b });
         }
+        try bufferedWriter.flush();
     }
 };
 
@@ -52,15 +94,26 @@ pub fn main() !void {
 
     // Fill the image with a gradient
     for (0..image_height) |y| {
+        std.debug.print("Row remaining {d}\n", .{image_height - y});
         for (0..image_width) |x| {
+            var r: f64 = @as(f64, @floatFromInt(x)) / @as(f64, @floatFromInt(image_width - 1));
+            var g: f64 = @as(f64, @floatFromInt(y)) / @as(f64, @floatFromInt(image_width - 1));
+            var b: f64 = 0;
+
+            r *= 255.999;
+            g *= 255.999;
+            b *= 255.999;
+
             const index = y * image_width + x;
+
             ppm.data[index].rgb = RGB{
-                .r = @intCast(x % 256),
-                .g = @intCast(y % 256),
-                .b = @intCast((x + y) % 256),
+                .r = @intFromFloat(r),
+                .g = @intFromFloat(g),
+                .b = @intFromFloat(b),
             };
         }
     }
+    std.debug.print("Done.      \n", .{});
 
     try ppm.saveInFile("test.ppm");
 }
